098628e6f67e85cf63c1a731aacabd4adf1e2330
Gladwin J <gladwinjoseph6174@protonmail.com>
2023-11-05 17:21:01 +0530

    Implement login functionality

--- restserver/views.py
+++ restserver/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Person
 
-# Create your views here.
+
+def loginApp(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        person = Person.objects.get(email=email, password=password)
+
+        if person is not None:
+            return HttpResponse({
+                "name": person.name,
+                "gender": person.gender,
+                "home_location": person.home_location,
+                "work_pattern": person.work_pattern,
+                "office": person.office.office_name,
+                "profile_photo": person.profile_photo,
+                "status": "success"
+                })
+        else:
+            return HttpResponse({"status": "fail", "message": "Invalid Credentials"})
